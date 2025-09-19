751af4fcee6ee4afeb851bda0f769d41c780d94d
Nensi Devani <ndevani894@rku.ac.in>
2025-09-19 10:15:42 +0530

    create splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../screens/welcome_screen.dart';
+import '../app_theme/app_colors.dart'; // Make sure the path is correct
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  // for delay
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => const WelcomeScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.accentBackground,
+      body: Center(
+        child: Image.asset(
+          '../assets/images/logo.png',
+          height: 150,
+          width: 150,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
