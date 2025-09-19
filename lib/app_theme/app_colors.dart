3b6bc292d28c4e5efea66e5abe207ed92d415930
Nensi Devani <ndevani894@rku.ac.in>
2025-09-19 10:15:10 +0530

    define colors

---
+++ lib/app_theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary button color
+  static const Color primaryButton = Color(0xFFB55266);
+
+  // Light background color (app base background)
+  static const Color lightBackground = Color(0xFFF7E6CD);
+
+  // Secondary button / dark background
+  static const Color secondaryBackground = Color(0xFFE7AC98);
+
+  // Accent background (search, widgets etc.)
+  static const Color accentBackground = Color(0xFFECCABF);
+}
