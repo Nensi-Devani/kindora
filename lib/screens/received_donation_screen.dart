ce87a4208601a7e1b579fbb1442bdf9d9d30b756
Nensi Devani <ndevani894@rku.ac.in>
2025-10-09 07:14:55 +0530

    created necessary screens

---
+++ lib/screens/received_donation_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReceivedDonationScreen extends StatefulWidget {
+  const ReceivedDonationScreen({super.key});
+
+  @override
+  State<ReceivedDonationScreen> createState() => _ReceivedDonationScreenState();
+}
+
+class _ReceivedDonationScreenState extends State<ReceivedDonationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
