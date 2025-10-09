ca7dffad095eba6409ea32301ab90477f7eccc67
Nensi Devani <ndevani894@rku.ac.in>
2025-10-09 10:57:49 +0530

    add payment done screen

---
+++ lib/screens/payment_done_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentDoneScreen extends StatefulWidget {
+  const PaymentDoneScreen({super.key});
+
+  @override
+  State<PaymentDoneScreen> createState() => _PaymentDoneScreenState();
+}
+
+class _PaymentDoneScreenState extends State<PaymentDoneScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
